b3b35d7fc662182412d9158ed049219d0481e518
johnnohj <166672127+johnnohj@users.noreply.github.com>
2024-08-28 15:16:16 -0400

    Update StreamBuffer.h

--- firmware/source/interfaces/StreamBuffer.h
+++ firmware/source/interfaces/StreamBuffer.h
@@ -8,7 +8,7 @@
 
 class StreamBuffer {
 public:
-    StreamBuffer(int streamBufferSize);
+    StreamBuffer(uint streamBufferSize);
     virtual ~StreamBuffer();
     inline uint32_t getAllocateSize() const {return static_cast<uint32_t>(_buffer.size()*4);}
     inline uint8_t* getDataPtr8() {return (uint8_t*)_buffer.data();}
@@ -18,7 +18,7 @@ public:
 protected:
     //std::vector<uint8_t> _buffer;
     std::vector<uint32_t> _buffer; // to be 32-bit aligned for ws2812b
-    int _bufSize;
+    uint _bufSize;
 };
 
 
